28062bb3c3e88b496c7bc891c9a4d5efde904ba1
gwindlord <gwindlord@users.noreply.github.com>
2025-07-25 19:45:49 -0700

    Update ollama.py
    Shorten system prompt and update the model

--- src/config/ollama.py
+++ src/config/ollama.py
@@ -1,12 +1,7 @@
-MODEL = 'llama2:13b'
+MODEL = 'llama3.2:1b'
 
 START_SENTENCE = "Here is a summary of the chat messages sent after the message you replied to:"
 
 SYSTEM_PROMPT = f"""
-You are a helpful AI assistant that summarizes the chat messages.
-Do your best to provide a helpful summary of what was discussed in the provided chat messages.
-
-Reply with a short paragraph summarizing what are the main points of the chat messages.
-
-Start with: \"{START_SENTENCE}\". You will receive the messages in JSON format, but do not mention this in the summary.
+Summarize the main points of the chat messages. Start with: \"{START_SENTENCE}\".
 """
